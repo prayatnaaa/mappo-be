71d42661ca33c1f27281983f19db124ce7aafcf4
Tude Prayatna <tudeprayatna04@gmail.com>
2025-04-12 08:11:25 +0800

    fix: add dotenv config to datasource

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,5 +1,8 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { join } from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
@@ -10,6 +13,7 @@ export const dataSourceOptions: DataSourceOptions = {
   database: process.env.DB_NAME,
   entities: [join(process.cwd(), 'dist/**/*.entity.js')],
   migrations: [join(process.cwd(), 'dist/db/migrations/*.js')],
+  synchronize: false,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
