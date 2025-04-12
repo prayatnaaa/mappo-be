110168b94b2548b3cab7f368b430718fa60d4ed1
Tude Prayatna <tudeprayatna04@gmail.com>
2025-04-12 21:32:03 +0800

    fix: adjust import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from 'db/data-source';
+import { dataSourceOptions } from '../db/data-source';
 import { PlacesModule } from './places/places.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(dataSourceOptions), PlacesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(dataSourceOptions),
+    PlacesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
