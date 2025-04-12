e83de3b042106a5c1af7c2d163d795ce468b7468
Tude Prayatna <tudeprayatna04@gmail.com>
2025-04-12 19:17:10 +0800

    feat(add): create endpoint for places

--- src/places/dto/create-place.dto.ts
+++ src/places/dto/create-place.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePlaceDto {}
+import { IsString } from 'class-validator';
+
+export class CreatePlaceDto {
+  @IsString()
+  public readonly name: string;
+
+  @IsString()
+  public readonly desc?: string;
+
+  public readonly latitude: number;
+  public readonly longitude: number;
+}
--- src/places/places.service.ts
+++ src/places/places.service.ts
@@ -1,26 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePlaceDto } from './dto/create-place.dto';
 import { UpdatePlaceDto } from './dto/update-place.dto';
+import { Repository } from 'typeorm';
+import { Place } from './entities/place.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class PlacesService {
-  create(createPlaceDto: CreatePlaceDto) {
-    return 'This action adds a new place';
+  constructor(
+    @InjectRepository(Place)
+    private readonly placeRepository: Repository<Place>,
+  ) {}
+
+  async create(createPlaceDto: CreatePlaceDto) {
+    const place = this.placeRepository.create(createPlaceDto);
+    return await this.placeRepository.save(place);
   }
 
-  findAll() {
-    return `This action returns all places`;
+  async findAll() {
+    return await this.placeRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} place`;
+  async findOne(id: number) {
+    const place = await this.placeRepository.findOne({
+      where: { id },
+    });
+
+    if (!place) {
+      throw new NotFoundException(place, 'Place is not available');
+    }
+
+    return place;
   }
 
-  update(id: number, updatePlaceDto: UpdatePlaceDto) {
-    return `This action updates a #${id} place`;
+  async update(id: number, updatePlaceDto: UpdatePlaceDto) {
+    const place = await this.findOne(id);
+    Object.assign(place, updatePlaceDto);
+    return await this.placeRepository.save(place);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} place`;
+  async remove(id: number) {
+    const place = await this.findOne(id);
+    return this.placeRepository.remove(place);
   }
 }
